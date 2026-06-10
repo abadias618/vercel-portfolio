65eb45a9ac794bec9bec8e002a7f5e514245b676
abdias <abadias618@gmail.com>
2026-06-10 17:57:21 -0600

    x

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,8 @@
 export const metadata = {
-  title: 'Portfolio - Home',
-  description: 'Home page of my portfolio.',
-}
+  title: "Portfolio - Home",
+  description: "Home page of my portfolio.",
+};
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  );
+  return <div>Home Abdias</div>;
 }
